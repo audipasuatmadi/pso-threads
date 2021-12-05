f3914e9c9ee1273b5c87328b2953f7a27dfe20f8
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-12-05 13:17:41 +0800

    [java] add implements thread version

--- java-language/src/com/audipasuatmadi/pso/main/Main.java
+++ java-language/src/com/audipasuatmadi/pso/main/Main.java
@@ -14,4 +14,24 @@ public class Main {
     System.out.println("[MAIN THREAD]: main thread mulai hidup lagi");
     System.out.printf("[MAIN THREAD]: program selesai jalan ser!");
   }
+
+// NOTE: uncomment kode di bawah dan komen kode di atas untuk mencoba versi Implements!
+//  public static void main(String[] args) throws InterruptedException {
+//        System.out.println("[MAIN THREAD]: hello ser! program mulai jalan");
+//        UserImplementRunnable userThread1 = new UserImplementRunnable("Seseorang A", 2, 400);
+//        UserImplementRunnable userThread2 = new UserImplementRunnable("Seseorang B", 3, 600);
+//
+//        Thread thread1 = new Thread(userThread1);
+//        Thread thread2 = new Thread(userThread2);
+//
+//        thread1.start();
+//        thread2.start();
+//
+//        System.out.println("[MAIN THREAD]: main thread menunggu 0.5 detik");
+//        Thread.sleep(500);
+//        System.out.println("[MAIN THREAD]: main thread menunggu 0.5 detik lagi");
+//        Thread.sleep(500);
+//        System.out.println("[MAIN THREAD]: main thread mulai hidup lagi");
+//        System.out.printf("[MAIN THREAD]: program selesai jalan ser!");
+//  }
 }
